dfc7299f7f5976596a6c7045f4e4d00b3ead0aeb
lfbear <lfbear@gmail.com>
2021-01-14 14:06:56 +0800

    Update k.sh

--- k.sh
+++ k.sh
@@ -1,15 +1,15 @@
 # switch k8s cluster quickly
 function k(){
-    kubectl="env kubectl"
+    kubectl="/usr/bin/env kubectl"
     if [ -z "$1" ];then
-        $kubectl config get-contexts
+        sh -c "$kubectl config get-contexts"
         return 0
     fi
 
-    $kubectl config use-context $1
+    sh -c "$kubectl config use-context $1"
     if [ "$?" -ne "0" ]; then
         echo "--- please select cluster from the following(NAME) ---"
-        $kubectl config get-contexts
+        sh -c "$kubectl config get-contexts"
     fi
     return 0
 }
