a5f01a1fb073442c738d959e4b34df6b88690a61
小飞熊 <lfbear@gmail.com>
2021-07-02 18:16:28 +0800

    Add CurrentContext to the bash PS1
    Signed-off-by: 小飞熊 <lfbear@gmail.com>

--- k.sh
+++ k.sh
@@ -3,6 +3,7 @@ function k(){
     kubectl="/usr/bin/env kubectl"
     if [ -z "$1" ];then
         sh -c "$kubectl config get-contexts"
+        show_context_in_ps1
         return 0
     fi
 
@@ -10,6 +11,22 @@ function k(){
     if [ "$?" -ne "0" ]; then
         echo "--- please select cluster from the following(NAME) ---"
         sh -c "$kubectl config get-contexts"
+    else
+      show_context_in_ps1
     fi
     return 0
+}
+
+export PS1_BACKUP=''
+function show_context_in_ps1(){
+  local curr_context=$(echo `kubectl config current-context`)
+  local context_name_template='\\[\\e[1;36m\\]'${curr_context}'\\[\\e[0m\\]' # you can change color here, attention: two backslash for a '\'
+  if [ -z "${PS1_BACKUP}" ];then
+    PS1_BACKUP=$PS1
+  fi
+  if [ -z "${curr_context}" ];then #recover PS1
+    PS1=${PS1_BACKUP}
+  else # set a new ps1
+    PS1=$(echo $PS1_BACKUP | sed 's%\\\$% '${context_name_template}' \\\$ %')
+  fi
 }
